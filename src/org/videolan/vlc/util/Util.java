32d31ed968d95d9d3862f148404b13bafe4f18a7
smyhvae <smyhvae@163.com>
2014-12-17 14:42:27 +0800

    只留下中文的国际化，将APP名字改为BMI

---
+++ src/org/videolan/vlc/util/Util.java
@@ -0,0 +1,89 @@
+/*****************************************************************************
+ * Util.java
+ *****************************************************************************
+ * Copyright © 2011-2014 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+package org.videolan.vlc.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.videolan.vlc.VLCApplication;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.widget.Toast;
+
+public class Util {
+    /** Print an on-screen message to alert the user */
+    public static void toaster(Context context, int stringId) {
+        Toast.makeText(context, stringId, Toast.LENGTH_SHORT).show();
+    }
+
+    public static int convertPxToDp(int px) {
+        DisplayMetrics metrics = VLCApplication.getAppResources().getDisplayMetrics();
+        float logicalDensity = metrics.density;
+        int dp = Math.round(px / logicalDensity);
+        return dp;
+    }
+
+    public static int convertDpToPx(int dp) {
+        return Math.round(
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                        VLCApplication.getAppResources().getDisplayMetrics())
+                );
+    }
+
+    public static String readAsset(String assetName, String defaultS) {
+        try {
+            InputStream is = VLCApplication.getAppResources().getAssets().open(assetName);
+            BufferedReader r = new BufferedReader(new InputStreamReader(is, "UTF8"));
+            StringBuilder sb = new StringBuilder();
+            String line = r.readLine();
+            if(line != null) {
+                sb.append(line);
+                line = r.readLine();
+                while(line != null) {
+                    sb.append('\n');
+                    sb.append(line);
+                    line = r.readLine();
+                }
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            return defaultS;
+        }
+    }
+
+    /**
+     * Get a resource id from an attribute id.
+     * @param context
+     * @param attrId
+     * @return the resource id
+     */
+    public static int getResourceFromAttribute(Context context, int attrId) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(new int[] {attrId});
+        int resId = a.getResourceId(0, 0);
+        a.recycle();
+        return resId;
+    }
+}
